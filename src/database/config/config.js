6e8f859944c80b740b29dcdac4e6a7082cb49d90
jbresky <joaquin_bresky@hotmail.com>
2022-04-24 18:31:26 -0300

    product detail casi terminado

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   "development": {
     "username": "root",
-    "password": null,
-    "database": "ecodev3",
+    "password": "joacosql2022",
+    "database": "ecodev",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
