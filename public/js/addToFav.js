1240697d848918cd7080e8dc8869bcafbb56e400
jbresky <joaquin_bresky@hotmail.com>
2022-05-17 10:35:42 -0300

    add to fav

--- public/js/addToFav.js
+++ public/js/addToFav.js
@@ -1,20 +1,3 @@
-document.addEventListener('DOMContentLoaded', function(){
-    
-
-    // async function postData(url){
-    //     const product = await fetch('/product', {
-    //         method: 'POST',
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         },
-    //         body: JSON.stringify({
-                
-    //         })
-    //     });
-    // }
-
- })
- 
  function addProductToFav(idProduct){
     fetch(`/api/user/product/${idProduct}/fav`)
     .then(res => res.json())
