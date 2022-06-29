a5269313e3be2f7d3030b4307ee676a965967ae0
jbresky <joaquin_bresky@hotmail.com>
2022-06-28 21:37:18 -0300

    addToCart function

--- public/js/addToCart.js
+++ public/js/addToCart.js
@@ -8,3 +8,14 @@ function addProductToCart(idProduct){
         console.log(err);
     })
 }
+
+function deleteProductInCart(idProduct){
+    fetch(`/api/product/cartDelete/${idProduct}`)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+        .catch(err => {
+            console.log(err);
+        })
+}
